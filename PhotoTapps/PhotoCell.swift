97b54576ccb1da0c090d560a26e080a2d9d3b6d8
Learn Swift <idev80299@gmail.com>
2024-09-01 14:33:07 +0500

    CollectionView Configuration

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Назар on 01.09.2024.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
---
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PhotosCollectionViewController.swift
+//  PhotoTapps
+//
+//  Created by Назар on 01.09.2024.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 15
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        
+        cell.backgroundColor = .black
+        
+        return cell
+    }
+}
+
+extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+}
